01319a1380bd184e3a4535a1585c081befe555f5
Jorge Payà <jorgepayaalbero@gmail.com>
2023-12-21 11:18:38 +0100

    Introduction.py file creation

---
+++ Introduction.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+
+
+# 1. Series
+# One-dimensional labeled data structure. It is similar to a 1D array in Numpy, but has an index that allows access to the values by label
+print('1. Series')
+serie = pd.Series([1, 2, 3, 4, 5]) # using Series class
+print(serie)
+# This will create a series with elements 1, 2, 3, 4 and 5. In addition, since we have not included information about the indexes, an automatic index is generated starting at 0.
+print('*****')
+serie = pd.Series([1, 2, 3, 4, 5], index=['a', 'b', 'c', 'd', 'e']) # using Series class
+print(serie)
+# the previous series has an index composed of letters.
+# In a series its elements can be accessed by index or by position (the latter is what we did in Numpy). Below are some operations that can be performed using the above series:
+print('*****')
+# Access the third element of the series
+print(serie["c"]) # Using index
+print(serie[2]) # Using position
+print('*****')
+# Change the value of the second element
+serie["b"] = 7 # Using index
+print(serie)
+print('*****')
+# Add 10 to all elements of the series
+serie = serie + 10 # also serie += 10
+print(serie)
+print('*****')
+# Calculate the sum of the elements of the series
+sum_all = serie.sum()
+print(sum_all)
+print('*****')
+# 2. DataFrame
+# Two-dimensional labeled data structure. It is similar to a 2D array in Numpy, but has an index that allows access to the values by label and rows and columns can be labeled.
+print('2. DataFrame')
+# dataframe = pd.DataFrame(data, index, columns)
+# data: data to be inserted in the dataframe
+# index: index of the dataframe
+# columns: columns of the dataframe
+# Let's create a dataframe using the DataFrame class:
+dataframe = pd.DataFrame([[1, 2, 3], [4, 5, 6], [7, 8, 9]]) # using DataFrame class, this will create a DataFrame with three rows and three columns each row.
+print(dataframe)
+print('*****')
+# create a new DataFrame with concrete indexes for rows and columns 
+data = {
+    "col A": [1, 2, 3],
+    "col B": [4, 5, 6],
+    "col C": [7, 8, 9]
+}
+
+dataframe = pd.DataFrame(data, index=["a", "b", "c"]) # Custom index is provided for the columns (labeling rows within a dictionary)
+print(dataframe)
+print('*****')
+# The above elements can be accessed by index or by position. Below are some operations that can be performed using the above dataframe:
+# Access all the data in a column
+print(dataframe["col A"]) # Using index
+
+
+
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Introduction to Pandas
+Pandas is an open source library that provides data structures and is designed to handle and analyze tabular data in Python. Pandas is based on Numpy, which allows it to integrate well into the data science ecosystem alongside other libraries such as Scikit-learn and Matplotlib.
+
+Pandas is a fundamental tool for any developer working with data in Python, as it provides a wide variety of tools for data exploration, cleaning and transformation, making the analysis process more efficient and effective. Specifically, the key points of this library are:
+
 - Data Structures
   - Series
   - DataFrame
