16aa025a4d67844d5e298591b20b63ea23c5f8a8
Jorge Payà <jorgepayaalbero@gmail.com>
2023-12-29 18:15:43 +0100

    Update Functions file

--- Functions.py
+++ Functions.py
@@ -13,4 +13,32 @@ print(d1)
 print(d2)
 print('*****')
 # Arithmetic operations
+print("Sum of series: ",s1.add(s2)) # also s1 + s2
+print("Sum of DataFrames: ",d1.add(d2))
+print('*****')
+# Statistical operations
+# They can be applied to both series and dataframes.
+# Pandas provide a large number of predefined functions that can be applied on the data structures seen above. Some of the most used in data analysis are:
+print("Mean: ",s1.mean())
+print("Median: ",s1.median())
+print("Number of elements: ",s1.count())
+print("Standard deviation: ",s1.std())
+print("Variance: ",s1.var())
+print("Maximum value: ",s1.max())
+print("Minimum value: ",s1.min())
+print("Correlation: ",s1.corr(s2))
+print("Statistic summary: ",s1.describe())
+print('*****')
+# Custom functions can also be applied to the data structures seen above. To do this, we have to program the function to receive a value (or a column or row in the case of a DataFrame) 
+# and return another modified one, and reference it with apply. In addition, this function allows using lambda expressions for the anonymous declaration of functions.
+s = pd.Series([1, 2, 3, 4, 5])
+print(s)
+# Explicit function
+def squared(x):
+    return x**2
+s1 = s.apply(squared)
+print(s1)
+# Lambda expression or anonymous function
+s2 = s.apply(lambda x: x**2)
+print(s2)
 
